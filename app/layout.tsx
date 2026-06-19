37d8f8437b0bc9e307947efee9527d4d1fd8305e
Phuong Nguyen <phuongsover2@gmail.com>
2026-06-20 02:01:12 +0900

    - Make sure the dark mode is displayed by default for entire app

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${poppins.variable} h-full antialiased`}>
+    <html lang="en" className={`${poppins.variable} dark h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
